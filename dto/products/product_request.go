91985038bd0d8d023fb24f037cb7c059478eee26
prasetioka <prasetiokusumaatmaja@gmail.com>
2022-12-04 08:25:22 +0700

    Server Side

--- dto/products/product_request.go
+++ dto/products/product_request.go
@@ -1,10 +1,10 @@
 package productsdto
 
 type CreateProductRequest struct {
-	Title string `gorm:"type: varchar(255)" json:"title"`
-	Price int    `gorm:"type: int" json:"email"`
-	Image string `gorm:"type: varchar(255)" json:"image"`
-	Qty   int    `gorm:"type: int" json:"qty"`
+	Title string `gorm:"type: varchar(255)" form:"title" json:"title"`
+	Price int    `gorm:"type: int" form:"price" json:"email"`
+	Image string `gorm:"type: varchar(255)" form:"image" json:"image"`
+	Qty   int    `gorm:"type: int" form:"qty" json:"qty"`
 }
 
 type UpdateProductRequest struct {
--- models/product.go
+++ models/product.go
@@ -2,8 +2,8 @@ package models
 
 type Product struct {
 	ID     int          `json:"id" gorm:"primary_key:auto_increment"`
-	Title  string       `json:"title" gorm:"type: varchar(255)"`
-	Price  int          `json:"price" gorm:"type: int"`
-	Image  string       `json:"image" gorm:"type: varchar(255)"`
-	Qty    int          `json:"qty" gorm:"type: varchar(255)"`
+	Title  string       `json:"title" form:"title" gorm:"type: varchar(255)"`
+	Price  int          `json:"price" form:"price" gorm:"type: int"`
+	Image  string       `json:"image" form:"image" gorm:"type: varchar(255)"`
+	Qty    int          `json:"qty" form:"qty" gorm:"type: varchar(255)"`
 }
